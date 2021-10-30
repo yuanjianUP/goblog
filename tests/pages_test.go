75c809f9e76c0402eb0836e6c6e8e37948e865e8
yuanjianUP <1587229752@qq.com>
2021-10-30 17:49:08 +0800

    完成测试代码

--- tests/pages_test.go
+++ tests/pages_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"net/http"
+	"strconv"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -11,11 +12,38 @@ func TestHomePage(t *testing.T) { //单元测试
 	baseURL := "http://localhost:3000"
 
 	//请求 -- 模拟用户访问浏览器
-	var (
-		resp *http.Response
-		err  error
-	)
-	resp, err = http.Get(baseURL + "/")
-	assert.NoError(t, err, "有错误发生,err不为空")
-	assert.Equal(t, 200, resp.StatusCode, "应用返回状态 200")
+	var tests = []struct {
+		method   string
+		url      string
+		expected int
+	}{
+		{"GET", "/", 200},
+		{"GET", "/about", 200},
+		{"GET", "/notfound", 404},
+		{"GET", "/aritcles", 200},
+		{"GET", "/aritcles/create", 200},
+		{"GET", "/aritcles/3", 200},
+		{"GET", "/aritcles/3/edit", 200},
+		{"POST", "/aritcles/3/edit", 200},
+		{"POST", "/aritcles", 200},
+		{"POST", "/aritcles/1/delete", 200},
+	}
+
+	for _, test := range tests {
+		t.Logf("当前请求URL:%v \n", test.url)
+		var (
+			resp *http.Response
+			err  error
+		)
+		switch {
+		case test.method == "POST":
+			data := make(map[string][]string)
+			resp, err = http.PostForm(baseURL+test.url, data)
+		default:
+			resp, err = http.Get(baseURL + test.url)
+		}
+		//断言
+		assert.NoError(t, err, "请求 "+test.url+" 时报错")
+		assert.Equal(t, test.expected, resp.StatusCode, test.url+"应返回状态码"+strconv.Itoa(test.expected))
+	}
 }
